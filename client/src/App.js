571bdfd4f26cb126d820b2dee7f7e2475dc8e78c
AnastasiaSN <65014597+AnastasiaSN@users.noreply.github.com>
2021-06-22 02:20:39 +0300

    add sign in, sign up, reset password pages, add click handling

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,10 @@ import ProfileSettingsPage from "./Components/Pages/ProfileSettingsPage";
 import ContactsPage from "./Components/Pages/ContactsPage";
 import GroupsPage from "./Components/Pages/GroupsPage";
 import AddContactPage from "./Components/Pages/AddContactPage";
+import SignUpPage from "./Components/Pages/SignUpPage";
+import ResetPasswordPage from "./Components/Pages/ResetPasswordPage";
+import SignInPage from "./Components/Pages/SignInPage";
+import NewPasswordPage from "./Components/Pages/NewPasswordPage";
 
 function App() {
     return (
@@ -16,6 +20,10 @@ function App() {
                     <Route path="/contacts" component={ContactsPage}/>
                     <Route path="/groups" component={GroupsPage}/>
                     <Route path="/add-contact" component={AddContactPage}/>
+                    <Route path="/sign-up" component={SignUpPage}/>
+                    <Route path="/sign-in" component={SignInPage}/>
+                    <Route path="/reset-password" component={ResetPasswordPage}/>
+                    <Route path="/new-password" component={NewPasswordPage}/>
                 </Switch>
             </Router>
         </div>
