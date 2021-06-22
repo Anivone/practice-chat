42391b51de5021fbdbc57d915d6584e9e23f2103
anvin23 <andrey1vnnik@gmail.com>
2021-06-23 02:08:19 +0300

    fix styles

--- client/src/Components/Pages/ContactsPage.js
+++ client/src/Components/Pages/ContactsPage.js
@@ -4,12 +4,20 @@ import MainNavbar from "../Common/MainNavbar";
 import ChatContainer from "../Common/ChatContainer";
 import {Box} from "@material-ui/core";
 import NavigationMenu from "../Common/NavigationMenu";
+import {makeStyles} from "@material-ui/core/styles";
 
+const useStyles = makeStyles(() => ({
+
+    fullHeight: {
+        height:'92vh'
+    }
+}))
 function ContactsPage(){
+    const classes = useStyles()
     return(
-        <div className="main-page">
+        <div className={classes.fullHeight}>
             <MainNavbar/>
-            <Box display="flex" height="50vw">
+            <Box display="flex" height={'100%'}>
                 <Box borderRight="1px solid #9fa8da" height="100%" width="25%">
                     <Box display="flex" height="100%" flexDirection="column" justifyContent="space-between">
                         <ContactsList/>
